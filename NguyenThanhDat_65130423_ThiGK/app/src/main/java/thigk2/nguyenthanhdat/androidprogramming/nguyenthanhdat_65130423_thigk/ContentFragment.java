428515bbc9a06fec9d7d6980d9cbff1b0295fcc7
Nguyễn Thành Đạt <datnguyen.2782005@gmail.com>
2026-04-20 07:52:31 +0700

    commit LẦN THỨ 6

--- NguyenThanhDat_65130423_ThiGK/app/src/main/java/thigk2/nguyenthanhdat/androidprogramming/nguyenthanhdat_65130423_thigk/ContentFragment.java
+++ NguyenThanhDat_65130423_ThiGK/app/src/main/java/thigk2/nguyenthanhdat/androidprogramming/nguyenthanhdat_65130423_thigk/ContentFragment.java
@@ -3,10 +3,12 @@ package thigk2.nguyenthanhdat.androidprogramming.nguyenthanhdat_65130423_thigk;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -14,51 +16,69 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class ContentFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public ContentFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ContentFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ContentFragment newInstance(String param1, String param2) {
-        ContentFragment fragment = new ContentFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_content, container, false);
+        View v = inflater.inflate(R.layout.fragment_footer, container, false);
+
+        // find view
+        Button b1 = v.findViewById(R.id.button1);
+        Button b2 = v.findViewById(R.id.button2);
+        Button b3 = v.findViewById(R.id.button3);
+        Button b4 = v.findViewById(R.id.button4);
+
+        //
+        FragmentManager fragmentManager = getParentFragmentManager();
+        // even
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // code
+                fragmentManager.beginTransaction()
+                        .replace(R.id.fragmentContainerView_Content, new VndToDolaFragment())
+                        .commit();
+            }
+        });
+
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // code
+                fragmentManager.beginTransaction()
+                        .replace(R.id.fragmentContainerView_Content, new DsCaKhucCMVNFragment())
+                        .commit();
+            }
+        });
+
+        b3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // code
+                fragmentManager.beginTransaction()
+                        .replace(R.id.fragmentContainerView_Content, new DsDanhNhanLsVnFragment())
+                        .commit();
+            }
+        });
+
+        b3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // code
+                fragmentManager.beginTransaction()
+                        .replace(R.id.fragmentContainerView_Content, new DsDanhNhanLsVnFragment())
+                        .commit();
+            }
+        });
+
+
+        return v;
     }
 }
